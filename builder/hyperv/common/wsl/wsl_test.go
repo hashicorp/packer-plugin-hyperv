359d9835c58c296b7f4d637be784b44d6b1fe089
Cameron Jack <camjjack@gmail.com>
2022-02-13 10:38:55 +1100

    Support for running within WSL2

---
+++ builder/hyperv/common/wsl/wsl_test.go
@@ -0,0 +1,55 @@
+package wsl
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetWSlTemp(t *testing.T) {
+
+	if !IsWSL() {
+		t.Skipf("not running in WSL")
+		return
+	}
+	tempDir, err := GetWSlTemp()
+	if err != nil {
+		t.Fatalf("should not have error: %s", err)
+	}
+	if tempDir == "" {
+		t.Fatalf("tempDir is not polulated correctly")
+	}
+}
+
+func TestConvertWindowsPathToWSlPath(t *testing.T) {
+
+	if !IsWSL() {
+		t.Skipf("not running in WSL")
+		return
+	}
+	wslPath, err := ConvertWindowsPathToWSlPath("C:\\Users\\User\\path with spaces")
+	if err != nil {
+		t.Fatalf("should not have error: %s", err)
+	}
+	if wslPath == "" {
+		t.Fatalf("wslPath is not polulated correctly")
+	}
+}
+
+func TestConvertWSlPathToWindowsPath(t *testing.T) {
+
+	if !IsWSL() {
+		t.Skipf("not running in WSL")
+		return
+	}
+	curDir, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("Getwd should not have error: %s", err)
+	}
+	winPath, err := ConvertWSlPathToWindowsPath(curDir)
+	if err != nil {
+		t.Fatalf("ConvertWSlPathToWindowsPath should not have error: %s", err)
+	}
+	if winPath == "" {
+		t.Fatalf("wslPath is not polulated correctly")
+	}
+}
