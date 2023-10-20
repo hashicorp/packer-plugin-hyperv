a3f1f40a1f837482bb0056f8a3bcc104f4abf382
remi-espie <remi.espie@etu.umontpellier.fr>
2023-10-20 09:54:37 +0200

    feat: solve tests fix: move math operation on float to work in powershell.go

--- builder/hyperv/common/powershell/hyperv/hyperv.go
+++ builder/hyperv/common/powershell/hyperv/hyperv.go
@@ -359,10 +359,10 @@ $vhdPath = Join-Path -Path "{{ .Path }}" -ChildPath "{{ .VHDX }}"
 
 Hyper-V\New-VM -Name "{{ .VMName }}" -Path "{{ .Path }}" -MemoryStartupBytes {{ .MemoryStartupBytes }} -VHDPath $vhdPath -SwitchName "{{ .SwitchName }}"
 {{- if eq .Generation 2}} -Generation {{ .Generation }} {{- end -}}
-{{- if ne .Version ""}} -Version {{ .Version }} {{- end }}
-{{ range $i, $switchName := .SwitchesNames -}}
+{{- if ne .Version ""}} -Version {{ .Version }} {{- end -}}
+{{ range $i, $switchName := .SwitchesNames }}
 Hyper-V\Add-VMNetworkAdapter -VMName "{{ $.VMName }}" -SwitchName "{{ $switchName }}" -StaticMacAddress "{{ index $.MacAddresses $i }}".Replace("-","")
-{{ end -}}
+{{- end -}}
 `))
 
 	var b bytes.Buffer
